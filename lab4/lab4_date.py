17cf736fa94b335ba1e412872129aa4b33647835
Mirzaakhmet <m_kulibay@kbtu.kz>
2025-02-10 17:15:35 +0500

    update

--- lab4/lab4_date.py
+++ lab4/lab4_date.py
@@ -10,4 +10,13 @@ print(f"\n2. Yesterday: {(datetime.now()).date() - timedelta(days=1)}\nToday: {(
 
 # Task 3
 
-print(f"\n3. First: {(datetime.now()).strftime('%Y-%m-%d %H:%M:%S')}")
+print(f"\n3. {(datetime.now()).strftime('%Y-%m-%d %H:%M:%S')}")
+
+#Task 4
+
+date1 = datetime(2025, 2, 9) # 9 февраль 2025
+date2 = datetime(2001, 9, 11)  # 11 сентября 2001
+
+raznica = date1 - date2
+print(f"\n4. Difference: {raznica}")
+print(f"All days: {raznica.days}")
