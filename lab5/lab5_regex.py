4e03b94f0061382410f3666965e30951c50200b5
Mirzaakhmet <m_kulibay@kbtu.kz>
2025-02-24 21:40:25 +0500

    update

---
+++ lab5/lab5_regex.py
@@ -0,0 +1,48 @@
+import re
+
+with open('"C:\Users\Asus\Documents\Little\PP2\lab5\row.txt"', 'r', encoding='utf-8') as f:
+    row = f.read()
+    
+print(row)
+
+#Task 1
+res = re.findall(r'a[b]*', row)
+print(res)
+
+#Task 2
+res = re.findall(r'ab{2,3}', row)
+print(res)
+
+#Task 3
+res = re.findall(r'[a-z]+(_[a-z]+)+', row)
+print(res)
+
+#Task 4
+res = re.findall(r'[A-Z][a-z]+', row)
+print(res)
+
+#Task 5
+res = re.findall(r'a.*b$', row)
+print(res)
+
+#Task 6
+res = re.sub(r'[\s,.]', ':', row)
+print(res)
+
+#Task 7
+camel_str = ''.join(word.capitalize() if i != 0 else word for i, word in enumerate(row.split('_')))
+print(camel_str)
+
+
+#Task 8
+result = re.split(r'(?=[A-Z])', row)
+result = [part for part in result if part]
+print(result)
+
+#Task 9
+res = re.sub(r'(?<=[a-z])(?=[A-Z])', ' ',row)
+print(res)
+
+#Task 10
+snake_str = re.sub(r'([a-z])([A-Z])', r'\1_\2', row).lower()
+print(snake_str)
