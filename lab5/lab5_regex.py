62f586a97c18f334de57b62e171e61c7b60b866d
Mirzaakhmet <m_kulibay@kbtu.kz>
2025-02-27 18:20:56 +0500

    update

--- lab5/lab5_regex.py
+++ lab5/lab5_regex.py
@@ -1,48 +1,48 @@
 import re
 
-with open('"C:\Users\Asus\Documents\Little\PP2\lab5\row.txt"', 'r', encoding='utf-8') as f:
+with open(r"C:\Users\Asus\Documents\Little\PP2\lab5\row.txt", 'r', encoding='utf-8') as f:
     row = f.read()
-    
+
 print(row)
 
-#Task 1
+# Task 1
 res = re.findall(r'a[b]*', row)
 print(res)
 
-#Task 2
+# Task 2
 res = re.findall(r'ab{2,3}', row)
 print(res)
 
-#Task 3
-res = re.findall(r'[a-z]+(_[a-z]+)+', row)
+# Task 3
+res = re.findall(r'[a-z]+(?:_[a-z]+)+', row)
 print(res)
 
-#Task 4
+# Task 4
 res = re.findall(r'[A-Z][a-z]+', row)
 print(res)
 
-#Task 5
-res = re.findall(r'a.*b$', row)
+# Task 5
+res = re.findall(r'a.*b$', row, re.MULTILINE)  
 print(res)
 
-#Task 6
-res = re.sub(r'[\s,.]', ':', row)
+# Task 6
+res = re.sub(r'[\s,.]+', ':', row)
 print(res)
 
-#Task 7
-camel_str = ''.join(word.capitalize() if i != 0 else word for i, word in enumerate(row.split('_')))
+# Task 7
+words = row.split('_')
+camel_str = words[0] + ''.join(word.capitalize() for word in words[1:])
 print(camel_str)
 
-
-#Task 8
+# Task 8
 result = re.split(r'(?=[A-Z])', row)
-result = [part for part in result if part]
+result = [part for part in result if part] 
 print(result)
 
-#Task 9
-res = re.sub(r'(?<=[a-z])(?=[A-Z])', ' ',row)
+# Task 9
+res = re.sub(r'(?<=[a-z])(?=[A-Z])', ' ', row)
 print(res)
 
-#Task 10
+# Task 10
 snake_str = re.sub(r'([a-z])([A-Z])', r'\1_\2', row).lower()
 print(snake_str)
